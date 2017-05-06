2460190e5f76507434e7a456d08494601078d090
Sharat Virunchipuram <sharatv@Sharats-MacBook-Pro.local>
2017-05-05 22:13:12 -0700

    initial commit

---
+++ app/components/app-component/app-component.vue
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+const AppComponent = Vue.component({
+  template: '<h1>Hello World</h1>',
+});
+
+export default AppComponent;
+
---
+++ app/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+import AppComponent from './components/app-component/app-component.vue';
+
+new Vue({
+  el: '#app',
+  components: {
+    'app-component': AppComponent
+  }
+});
+
